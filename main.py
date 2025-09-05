c41d2a903b7a279149e7cfd00a1efcfcf95dc81d
John <johannesdejenu3@gmail.noreply.github.com>
2025-09-06 00:02:21 +0300

    adding the menu, resources and the todo list for the coffee machine

---
+++ main.py
@@ -0,0 +1,36 @@
+MENU = {
+    "espresso": {
+        "ingredients": {
+            "water": 50,
+            "coffee": 18,
+        },
+        "cost": 1.5,
+    },
+    "latte": {
+        "ingredients": {
+            "water": 200,
+            "milk": 150,
+            "coffee": 24,
+        },
+        "cost": 2.5,
+    },
+    "cappuccino": {
+        "ingredients": {
+            "water": 250,
+            "milk": 100,
+            "coffee": 24,
+        },
+        "cost": 3.0,
+    }
+}
+
+resources = {
+    "water": 300,
+    "milk": 200,
+    "coffee": 100,
+}
+# TODO 1 : print report
+# TODO 2 : check resources sufficiency
+# TODO 3 : process coins
+# TODO 4 : check transaction successful
+# TODO 5: make coffee
