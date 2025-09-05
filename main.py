b7c063a33d0fd4f2cc7e53a62209741048b808b5
John <johannesdejenu3@gmail.noreply.github.com>
2025-09-06 01:07:43 +0300

    done with the TODO, now on finalizing and trying to squash the bug i encountered

--- main.py
+++ main.py
@@ -34,35 +34,51 @@ resources = {
 # TODO 3 : process coins
 # TODO 4 : check transaction successful
 # TODO 5: make coffee
-user_input = input("What would you like? (espresso/latte/cappuccino): ").strip()
-money = 0
-def report():
-    print(f"Water : {resources["water"]}ml")
-    print(f"Milk : {resources["milk"]}ml")
-    print(f"Coffee : {resources["coffee"]}g")
-    print(f"Money : ${money}")
-if user_input == "report":
-    report()
-if user_input in MENU:
-    for i in MENU["latte"]["ingredients"]:
-        if resources["water"] < MENU[user_input]["ingredients"]["water"]:
-            print("Sorry there is not enough water.")
-        elif resources["milk"] < MENU[user_input]["ingredients"]["milk"]:
-            print("Sorry there is not enough milk.")
-        elif resources["coffee"] < MENU[user_input]["ingredients"]["coffee"]:
-            print("Sorry there is not enough coffee.")
-def process_coins():
-    print("Please insert coins.")
-    quarter = int(input("how many quarters?: "))
-    dime = int(input("how many dimes?: "))
-    nickle = int(input("how many nickles?: "))
-    penny = int(input("how many pennies?: "))
-    return quarter, dime, nickle, penny
-quarters, dimes, nickles, pennies = process_coins()
-total = 0.25 * quarters + 0.1 * dimes + 0.05 * nickles + 0.01 * pennies
-total = round(total, 2)
-if total < MENU[user_input]["cost"]:
-    print("Sorry that's not enough. Money refunded.")
-else:
-    money += total
+want_coffee = True
+while want_coffee:
+    user_input = input("What would you like? (espresso/latte/cappuccino): ").strip()
+    money = 0
+    total = 0
+    def report():
+        print(f"Water : {resources["water"]}ml")
+        print(f"Milk : {resources["milk"]}ml")
+        print(f"Coffee : {resources["coffee"]}g")
+        print(f"Money : ${money}")
+    if user_input == "off":
+        want_coffee = False
+    elif user_input == "report":
+        report()
+    elif user_input in MENU:
+        for i in MENU[user_input]["ingredients"]:
+            if resources["water"] < MENU[user_input]["ingredients"]["water"]:
+                print("Sorry there is not enough water.")
+                want_coffee = False
+            elif resources["milk"] < MENU[user_input]["ingredients"]["milk"]:
+                print("Sorry there is not enough milk.")
+                want_coffee = False
+            elif resources["coffee"] < MENU[user_input]["ingredients"]["coffee"]:
+                print("Sorry there is not enough coffee.")
+                want_coffee = False
+        resources["water"] = resources["water"] - MENU[user_input]["ingredients"]["water"]
+        resources["milk"] = resources["milk"] - MENU[user_input]["ingredients"]["milk"]
+        resources["coffee"] = resources["coffee"] - MENU[user_input]["ingredients"]["coffee"]
+        def process_coins():
+            print("Please insert coins.")
+            quarter = int(input("how many quarters?: "))
+            dime = int(input("how many dimes?: "))
+            nickle = int(input("how many nickles?: "))
+            penny = int(input("how many pennies?: "))
+            return quarter, dime, nickle, penny
+        quarters, dimes, nickles, pennies = process_coins()
+        total += 0.25 * quarters + 0.1 * dimes + 0.05 * nickles + 0.01 * pennies
+        total_1 = round(total, 2)
+        money += total_1
+        change = total_1 - MENU[user_input]["cost"]
+        change = round(change, 2)
+        if total_1 < MENU[user_input]["cost"]:
+            print("Sorry that's not enough. Money refunded.")
+        else:
+            if total_1 > MENU[user_input]["cost"]:
+                print(f"Here is ${change} dollars in change.")
+
 
